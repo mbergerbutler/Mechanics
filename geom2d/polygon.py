169c98d0cccfd6b0c5136ec5d50cc25cc261d3c8
mbergerbutler <max.bergerbutler@gmail.com>
2022-06-25 16:26:28 -0400

    Adding polygon code

--- geom2d/polygon.py
+++ geom2d/polygon.py
@@ -1,4 +1,9 @@
+import operator
+from functools import reduce
+
 from geom2d.point import Point
+from geom2d.segment import Segment
+from utils.pairs import make_round_pairs
 
 
 class Polygon:
@@ -6,3 +11,19 @@ class Polygon:
         if len(vertices) < 3:
             raise ValueError('Need 3 or more vertices')
         self.vertices = vertices
+
+    def sides(self):
+        vertex_pairs = make_round_pairs(self.vertices)
+        return [
+            Segment(pair[0], pair[1])
+            for pair in vertex_pairs
+        ]
+
+    @property
+    def centroid(self):
+        vtx_count = len(self.vertices)
+        vtx_sum = reduce(operator.add, self.vertices)
+        return Point(
+            vtx_sum.x / vtx_count,
+            vtx_sum.y / vtx_count
+        )
